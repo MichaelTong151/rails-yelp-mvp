c3154584a219071d9522f8bf06e077f43cc3a9c3
Michael Tong <michi.tong@gmail.com>
2023-11-22 22:50:57 +0100

    Yelp-mvp incomplete controller  assign and route

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,52 +1,49 @@
 class RestaurantsController < ApplicationController
+
   def index
-     @restaurants = Restaurant.all
-     render json: @restaurants
+    @restaurants = Restaurant.all
   end
 
   def show
-     @restaurant = Restaurant.find(params[:id])
-     render json: @restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
-     @restaurant = Restaurant.new
+    @restaurant = Restaurant.new
   end
 
   def create
-     @restaurant = Restaurant.new(restaurant_params)
-
-     if @restaurant.save
-       render json: @restaurant, status: :created
-     else
-       render json: @restaurant.errors, status: :unprocessable_entity
-     end
+    @restaurant = Restaurant.new(params.require(:restaurant).permit(:name, :address, :phone_number, :category))
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+
   def edit
-     @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
      @restaurant = Restaurant.find(params[:id])
-
      if @restaurant.update(restaurant_params)
        render json: @restaurant
      else
        render json: @restaurant.errors, status: :unprocessable_entity
      end
-  end
 
-  def destroy
-     @restaurant = Restaurant.find(params[:id])
-     @restaurant.destroy
-
-     render json: { message: 'Successfully deleted', status: :ok }
-  end
+      def destroy
+      restaurant = Restaurant.find(params[:id])
+      restaurant.destroy
+      redirect_to restaurants_path, status: :see_other
+      end
 
   private
 
   def restaurant_params
-     params.require(:restaurant).permit(:name, :description, :rating)
+     params.require(:restaurant).permit(:name, :address, :rating)
   end
- end
+end
+end
